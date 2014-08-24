847f9880382afbdb204aa46ed19e5ce18deaf3b6
Jack Peterson <jack@tinybike.net>
2014-08-23 23:17:48 -0700

    packaging stuff

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup
 
 setup(
     name="Coinbridge",
@@ -12,8 +9,12 @@ setup(
     author_email="<jack@dyffy.com>",
     maintainer="Jack Peterson",
     maintainer_email="<jack@dyffy.com>",
+    license="MIT",
     url="https://github.com/tensorjack/coinbridge",
+    download_url = 'https://github.com/tensorjack/coinbridge/tarball/0.1',
     packages=["coinbridge"],
+    include_package_data=True,
     package_data={"coinbridge": ["./data/*.json", "./bitcoin-listen"]},
-    install_requires=["sqlalchemy", "psycopg2", "bunch", "python-jsonrpc"]
-    )
+    install_requires=["sqlalchemy", "psycopg2", "bunch", "python-jsonrpc"],
+    keywords = ["bitcoin", "postgres", "transaction", "bridge"]
+)
